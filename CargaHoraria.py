d1d40083afa75e1458cc2857bf9cdc2f86f71db3
Code-Diego <thewolf2108@gmail.com>
2023-03-22 02:02:47 -0500

    add data 23-1 and fixing code this data

--- CargaHoraria.py
+++ CargaHoraria.py
@@ -33,47 +33,47 @@ def splitList(list,condition):
     return [ l.split(condition) for l in list]
 
 def data(): 
-    #Creamos un Data Frame como BD
-    dfc = pd.read_excel('./data/Horarios2022-2.xlsx')
-    #Configuracion a 'pd' para mostrar todas las filas de la data
+    # Creamos un Data Frame como BD
+    dfc = pd.read_excel('./data/Horarios2023-1.xlsx')
+    # Configuracion a 'pd' para mostrar todas las filas de la data
     pd.set_option('display.max_rows', None)
-    #Titulos de Data Frame
+    # Titulos de Data Frame
     names = dfc.columns.tolist() #['CURSOS'(0), 'CÓDIGO'(1), 'HORARIO'(2), 'AULA'(3), 'DOCENTE'(4), 'N'(5)] 
-    #Eliminamos los titulos extras, y datos inecesaria en la data
+    # Eliminamos los titulos extras, y datos inecesaria en la data
     dfc = deleteExtraTitles(dfc, names)
-    txt1 = 'CURSOS OFRECIDOS EN EL PERIODO ACADÉMICO 2022-2'
+    txt1 = 'CURSOS OFRECIDOS EN EL PERIODO ACADÉMICO'
     txt2 = 'CURSOS ELECTIVOS'
     txt3 = 'ESCUELA PROFESIONAL DE '
-    dfc = deleteNoDataWithText(dfc,txt1)
+    dfc = deleteNoDataWithTextStart(dfc,txt1)
     dfc = deleteNoDataWithText(dfc,txt2)
     dfc = deleteNoDataWithTextStart(dfc,txt3)
     
-    #Creando una lista para cada columna del Data Frame
+    # Creando una lista para cada columna del Data Frame
     cursos, codigos, horarios, aulas, docentes, ns = [dfc[name].tolist() for name in names]
 
-    #Limpiamos los "nan" de Data Frame
+    # Limpiamos los "nan" de Data Frame
     dfc[names[0]] = deleteNan(cursos)
     dfc[names[1]] = deleteNan(codigos)
     dfc[names[5]] = deleteNan(ns)
 
-    #Para 'ns' cambiamos los datos float -> int
+    # Para 'ns' cambiamos los datos float -> int
     dfc[names[5]] = dfc[names[5]].astype(int)
 
-    #Renombrando 'nan' por falda de datos (N.D.)
+    # Renombrando 'nan' por falda de datos (N.D.)
     dfc[names[2]] = dfc[names[2]].fillna('n.d.')
     dfc[names[3]] = dfc[names[3]].fillna('n.d.')
     dfc[names[4]] = dfc[names[4]].fillna('n.d.')
 
-    #Actualizando xd
+    # Actualizando xd
     cursos, codigos, horarios, aulas, docentes, ns = [dfc[name].tolist() for name in names]
 
-    #Separando en sublistas las sig listas
-    dfc[names[1]] = splitList(codigos,' ')
+    # Separando en sublistas las sig listas
+    dfc[names[1]] = splitList(codigos,'\n')
     dfc[names[2]] = splitList(horarios,'\n')
     dfc[names[4]] = splitList(docentes,'\n')
-    dfc[names[3]] = splitList(aulas,' ') #Data split : bad split for the formant in main data excel
+    dfc[names[3]] = splitList(aulas,'/') #Data split : bad split for the formant in main data excel
 
-    #Actulizacion again xd
+    # Actulizacion again xd
     cursos, codigos, horarios, aulas, docentes, ns = [dfc[name].tolist() for name in names]
     
     return dfc    
--- graficaTK.py
+++ graficaTK.py
@@ -1,38 +1,39 @@
 import tkinter as tk
+import CargaHoraria as ch
 
-#creamos una ventana
+# creamos una ventana
 root = tk.Tk()
 root.title("Generador de Horarios")
 
-# Obtiene las dimensiones de la pantalla
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+# Dimensiones
+screen_width = 1500
+screen_height = 800
 
-# Establece las dimensiones de la ventana para que ocupe toda la pantalla
+# establece las dimensiones de la ventana para que ocupe toda la pantalla
 root.geometry(f"{screen_width}x{screen_height}+0+0")
 
-#'label' para mostrar una etiqueta
-title_label = tk.Label(root, text="Generador de Horarios")
+# 'label' para mostrar una etiqueta
+title_label = tk.Label(root, text="Generador Horario de la U :D")
 title_label.pack()
 
-#'Listbox' para mostrar los cursos disponibles
-courses = ['Curso 1', 'Curso 2', 'Curso 3']
-course_listbox = tk.Listbox(root, selectmode=tk.SINGLE)
+# 'Listbox' para mostrar los cursos disponibles
+courses = ch.cursos_sec()
+course_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
 for course in courses:
     course_listbox.insert(tk.END, course)
-course_listbox.pack()
+course_listbox.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-#'Button' para que el usuario pueda generar el horario:
+# 'Button' para que el usuario pueda generar el horario:
 generate_button = tk.Button(root, text="Generar Horario")
 generate_button.pack()
 
-#usomos nuestra data
+# usomos nuestra data
 def generate_schedule():
     selected_course = course_listbox.get(course_listbox.curselection())
     # Utiliza los datos de Excel para generar el horario
 
-#asocia la función generate_schedule con el botón Generar Horario
+# asocia la función generate_schedule con el botón Generar Horario
 generate_button.config(command=generate_schedule)
 
-#finalmente, inicia el bucle principal de la aplicacion
+# finalmente, inicia el bucle principal de la aplicacion
 root.mainloop()
