117fcc46300affba850b22f56e937260a1b72693
Code-Diego <thewolf2108@gmail.com>
2023-03-25 23:09:07 -0500

    diccionario para CargaHorario

--- CargaHoraria.py
+++ CargaHoraria.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
-def deleteExtraTitles(df, condition):
+def delete_extra_titles(df, condition):
     iters = [ index for index,row in df.iterrows() if row[0] == condition[0] ]
     df = df.drop(i for i in iters)
     df = df.reset_index(drop=True)
     return df
 
-def deleteNoDataWithText(df,txt):
+def delete_data_with(df,txt):
     iters = [ index for index,row in df.iterrows() if row[0] == txt ]
     df = df.drop(i for i in iters)
     df = df.reset_index(drop=True)
     return df
 
-def deleteNoDataWithTextStart(df,txt):
+def delete_data_starts_with(df,txt):
     iters = [ index for index,row in df.iterrows() if not pd.isna(row[0]) and str(row[0]).startswith(txt) ]
     df = df.drop(i for i in iters)
     df = df.reset_index(drop=True)
     return df
 
-def deleteNan(list):
+def delete_nan(list):
     newList, last, = [], ''
     for l in list :
         if pd.isna(l) : l = last
@@ -26,83 +26,122 @@ def deleteNan(list):
         newList.append(l)
     return newList      
 
-def renameNan(list):
-    return [ 'N.D' if pd.isna(l) else l for l in list]
-
-def splitList(list,condition):
+def split_list(list,condition):
     return [ l.split(condition) for l in list]
 
-def data(): 
-    # Creamos un Data Frame como BD
-    dfc = pd.read_excel('./data/Horarios2023-1.xlsx')
-    # Configuracion a 'pd' para mostrar todas las filas de la data
-    pd.set_option('display.max_rows', None)
-    # Titulos de Data Frame
-    names = dfc.columns.tolist() #['CURSOS'(0), 'CÓDIGO'(1), 'HORARIO'(2), 'AULA'(3), 'DOCENTE'(4), 'N'(5)] 
+def read_data(): 
+    dfc = pd.read_excel('./data/Horarios2023-1.xlsx') # Data Frame
+    pd.set_option('display.max_rows', None) # Confi para mostrar todas las filas
+    titles = dfc.columns.tolist() #['CURSOS'(0), 'CÓDIGO'(1), 'HORARIO'(2), 'AULA'(3), 'DOCENTE'(4), 'N'(5)] 
+    
     # Eliminamos los titulos extras, y datos inecesaria en la data
-    dfc = deleteExtraTitles(dfc, names)
     txt1 = 'CURSOS OFRECIDOS EN EL PERIODO ACADÉMICO'
     txt2 = 'CURSOS ELECTIVOS'
-    txt3 = 'ESCUELA PROFESIONAL DE '
-    dfc = deleteNoDataWithTextStart(dfc,txt1)
-    dfc = deleteNoDataWithText(dfc,txt2)
-    dfc = deleteNoDataWithTextStart(dfc,txt3)
+
+    txt3 = 'ESCUELA PROFESIONAL DE ' ##IMPORTANTE PARA SEPARAR
+
+    dfc = delete_extra_titles(dfc, titles)
+    dfc = delete_data_starts_with(dfc,txt1)
+    dfc = delete_data_with(dfc,txt2)
+    dfc = delete_data_starts_with(dfc,txt3)
     
     # Creando una lista para cada columna del Data Frame
-    cursos, codigos, horarios, aulas, docentes, ns = [dfc[name].tolist() for name in names]
+    cursos, codigos, horarios, aulas, docentes, ns = [dfc[title].tolist() for title in titles]
 
     # Limpiamos los "nan" de Data Frame
-    dfc[names[0]] = deleteNan(cursos)
-    dfc[names[1]] = deleteNan(codigos)
-    dfc[names[5]] = deleteNan(ns)
+    dfc[titles[0]] = delete_nan(cursos)
+    dfc[titles[1]] = delete_nan(codigos)
+    dfc[titles[5]] = delete_nan(ns)
 
     # Para 'ns' cambiamos los datos float -> int
-    dfc[names[5]] = dfc[names[5]].astype(int)
+    dfc[titles[5]] = dfc[titles[5]].astype(int)
 
     # Renombrando 'nan' por falda de datos (N.D.)
-    dfc[names[2]] = dfc[names[2]].fillna('n.d.')
-    dfc[names[3]] = dfc[names[3]].fillna('n.d.')
-    dfc[names[4]] = dfc[names[4]].fillna('n.d.')
+    dfc[titles[2]] = dfc[titles[2]].fillna('n.d.')
+    dfc[titles[3]] = dfc[titles[3]].fillna('n.d.')
+    dfc[titles[4]] = dfc[titles[4]].fillna('n.d.')
 
     # Actualizando xd
-    cursos, codigos, horarios, aulas, docentes, ns = [dfc[name].tolist() for name in names]
+    cursos, codigos, horarios, aulas, docentes, ns = [dfc[title].tolist() for title in titles]
 
     # Separando en sublistas las sig listas
-    dfc[names[1]] = splitList(codigos,'\n')
-    dfc[names[2]] = splitList(horarios,'\n')
-    dfc[names[4]] = splitList(docentes,'\n')
-    dfc[names[3]] = splitList(aulas,'/') #Data split : bad split for the formant in main data excel
+    dfc[titles[1]] = split_list(codigos,'\n')
+    dfc[titles[2]] = split_list(horarios,'\n')
+    dfc[titles[4]] = split_list(docentes,'\n')
+    dfc[titles[3]] = split_list(aulas,'/') #Data split : bad split for the formant in main data excel
 
     # Actulizacion again xd
-    cursos, codigos, horarios, aulas, docentes, ns = [dfc[name].tolist() for name in names]
+    cursos, codigos, horarios, aulas, docentes, ns = [dfc[title].tolist() for title in titles]
     
     return dfc    
 
-#Esto podia meterlo a una clase Data :0
-def cursos_sec():
-    df = data()
-    cursos, codigos = [], []
-    for index,row in df.iterrows():
-        if (row[0] not in cursos) or (row[1][0] not in codigos) : 
-            cursos.append( row[0] )
-            codigos.append( row[1][0] )
-    return cursos
+def cursos_of(df):
+        cursos, codigos = [], []
+        for index,row in df.iterrows():
+            if (row[0] not in cursos) or (row[1][0] not in codigos) : 
+                cursos.append( row[0] )
+                codigos.append( row[1][0] )
+        return cursos
     
-def codigos_sec():
-    df = data()
-    codigos = [ codigo for codigo,seccion in df[df.columns.tolist()[1]] ]
-    secciones = [ seccion for codigo,seccion in df[df.columns.tolist()[1]] ]
+def codigos_of(df):
+        codigos = [ codigo for codigo,seccion in df[df.columns.tolist()[1]] ]
+        secciones = [ seccion for codigo,seccion in df[df.columns.tolist()[1]] ]
 
-    newCodigos =  []
-    for codigo in codigos :
-        if codigo not in newCodigos:
-            newCodigos.append(codigo)
-    return newCodigos
+        newCodigos =  []
+        for codigo in codigos :
+            if codigo not in newCodigos:
+                newCodigos.append(codigo)
+        return newCodigos
 
-def diccionario():
-    cursos = cursos_sec()
-    codigos = codigos_sec()
-    diccionario = {codigos[i]:cursos[i] for i in range(len(codigos))}
-    return diccionario
+class DataBase :
+    def __init__(self,data= read_data()):
+        self.data = data
+
+    def get_data(self): 
+        return self.data
     
+    def get_cursos_names(self):
+        return cursos_of(self.data)
+    
+    def get_codigos_names(self):
+        return codigos_of(self.data)
+    
+#Structure
+c = {
+        'bf01': {
+            'curso': 'fisica',
+            'codigo' : 'bf01',
+            'seccion': {
+                'A': {
+                    'aula': 'A-101',
+                    'docente': 'Juan Perez',
+                    'horario': 'LU 8:00-10:00'
+                },
+                'B': {
+                    'aula': 'A-101',
+                    'docente': 'Juan Perez',
+                    'horario': 'MA 8:00-10:00'
+                }
+            }
+        }
+    }
+  
+#Creando un diccionrio con los datos de la data
+def dataframe_to_dict():
+    df = read_data()
+    dictionary = { codigo:{} for codigo in codigos_of(df) }
+    for row in df.iterrows():
+        dictionary[row[1][1][0]].update({'curso':row[1][0]})
+        dictionary[row[1][1][0]].update({'codigo':row[1][1][0]})
+        dictionary[row[1][1][0]].update({'seccion':{}})
+    for row in df.iterrows():   
+        dictionary[row[1][1][0]]['seccion'].update({row[1][1][1]:{}})
+        dictionary[row[1][1][0]]['seccion'][row[1][1][1]].update({'aula':row[1][3][0]})
+        dictionary[row[1][1][0]]['seccion'][row[1][1][1]].update({'docente':row[1][4][0]})
+        dictionary[row[1][1][0]]['seccion'][row[1][1][1]].update({'horario':row[1][2][0]})
+        
+    return dictionary
+        
 
+if __name__ == '__main__':
+    dataframe_to_dict()
--- graficaTK.py
+++ graficaTK.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import CargaHoraria as ch
+from CargaHoraria import DataBase
 
 # creamos una ventana
 root = tk.Tk()
@@ -17,11 +17,10 @@ root.rowconfigure(2, weight=1)
 root.columnconfigure(0, weight=3)
 root.columnconfigure(1, weight=2)
 
-# After functions 
+# Filtro
 def search_courses():
     search_term = search_entry.get().lower()  # Obtener el término de búsqueda y convertirlo a minúsculas
     course_listbox.delete(0, tk.END)  # Eliminar los cursos actuales en el Listbox
-    
     # Agregar los cursos que coinciden con el término de búsqueda al Listbox
     for course in courses:
         if search_term in course.lower():
@@ -29,28 +28,32 @@ def search_courses():
 
 # Widgets :
 search_entry = tk.Entry(root, width=30)
-courses = ch.cursos_sec()
+courses = DataBase.get_cursos_names()
 course_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
 for course in courses:
     course_listbox.insert(tk.END, course)
+scrolbar_course = tk.Scrollbar(root, command=course_listbox.yview)
+course_listbox.config(yscrollcommand=scrolbar_course.set)
 course_selected= tk.Listbox(root)
 button_search = tk.Button(root, text="Buscar", command=search_courses)
-button_generate = tk.Button(root, text="Generar Horario")
+
 
 # Grid :
 search_entry.grid   (row=0, column=0, sticky='ew', ipadx=10, ipady=10 , padx=(50,120), pady=(80,5))
 button_search.grid  (row=0, column=0, sticky='e', ipadx=10, ipady=10, padx=50, pady=(80,5))
-course_listbox.grid (row=1, column=0, sticky='nsew', padx=50, pady=(5,5))
+course_listbox.grid (row=1, column=0, sticky='nsew', padx=5, pady=(5,5))
+scrolbar_course.grid(row=1, column=0, sticky='nse', padx=(5,5), pady=(5,5))
 course_selected.grid(row=0, column=1, sticky='nsew', padx=50, pady=(120,5), rowspan=2)
-button_generate.grid(row=2, column=0, sticky='', ipadx=40, ipady=20, padx=5, pady=5, columnspan=2)
 
-# Before functions
+
+# Generar el horario
 def generate_schedule():
     selected_course = course_listbox.get(course_listbox.curselection())
+    print(selected_course)
     # Utiliza los datos de Excel para generar el horario
 
-# asocia la función generate_schedule con el botón Generar Horario
-button_generate.config(command=generate_schedule)
+button_generate = tk.Button(root, text="Generar Horario",command=generate_schedule)
+button_generate.grid(row=2, column=0, sticky='', ipadx=40, ipady=20, padx=5, pady=5, columnspan=2)
 
 # finalmente, inicia el bucle principal de la aplicacion
 root.mainloop()
