21c09f41e2a9599b0b2b199dd49fa4387c2d06d1
root <root@DESKTOP-ATIFA3B>
2024-03-08 03:19:52 -0500

    script temporal mientras no encuentro una solu para CORS

--- web/script.js
+++ web/script.js
@@ -1,22 +1,24 @@
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        //recorriendo todo la data
-        for (let key in data) {
-            if (data.hasOwnProperty(key)) {
-                console.log("Key : ", key)
-                let seccion = data[key].seccion;
-                for (let subKey in seccion) {
-                    if (seccion.hasOwnProperty(subKey)) {
-                        console.log("subKey : ", subKey);
-                        console.log("Horario:", seccion[subclave].horario);
-                        console.log("Aula:", seccion[subclave].aula);
-                        console.log("Docente:", seccion[subclave].docente);
-                    }
-                }
-            }
-        }
-    })
-    .catch(error => {
-        console.error("error!! : ", error);
-    })
+// Mientras no encuentro una solucion para CORS
+
+// fetch('https://github.com/code-diego/Horario/blob/main/web/data.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         //recorriendo todo la data
+//         for (let key in data) {
+//             if (data.hasOwnProperty(key)) {
+//                 console.log("Key : ", key)
+//                 let seccion = data[key].seccion;
+//                 for (let subKey in seccion) {
+//                     if (seccion.hasOwnProperty(subKey)) {
+//                         console.log("subKey : ", subKey);
+//                         console.log("Horario:", seccion[subKey].horario);
+//                         console.log("Aula:", seccion[subKey].aula);
+//                         console.log("Docente:", seccion[subKey].docente);
+//                     }
+//                 }
+//             }
+//         }
+//     })
+//     .catch(error => {
+//         console.error("error!! : ", error);
+//     })
