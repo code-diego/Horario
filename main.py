e37879b1b6cebec3d091b83919b9138f21ebad02
Diego LAP <thewolf2108@gmail.com>
2024-08-04 12:50:57 -0500

    Cambia a la datos(json) para el ciclo 24-2

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from CargaHoraria import DataBase 
 from CargaHoraria import dataframe_to_dict
 from scraping import url_to_df
 import json
 
 def main():
-    database = DataBase()
     dataframe = url_to_df()
     dict_data = dataframe_to_dict(dataframe)
     
-    print(json.dumps(dict_data, indent=4))
+    print(dict_data)
 
 if __name__ == "__main__":
     main()
--- toJson.py
+++ toJson.py
@@ -1,13 +1,16 @@
-from CargaHoraria import DataBase 
+from CargaHoraria import DataBase
+from CargaHoraria import dataframe_to_dict
+from scraping import url_to_df 
 import json
 import os
 
 # diccionario (python) to json (para javascript)
 
 def run():
-    # crea una variable de tipo DataBase(CargaHoraria.py)
-    data = DataBase()
-    diccionario = data.get_diccionario()
+    # creamos una varaible data con dataframe de la url (scraping.py)
+    data = url_to_df()
+    # Lo pasamos a un diccionario (CargaHoraria.py)
+    diccionario = dataframe_to_dict(data)
 
     # asegura que exista la carpeta 'docs'
     if not os.path.exists('docs'):
