014c6d1fc34deb7793ef8cb344e277e56a51b3a9
DiegoPC <thewolf2108@gmail.com>
2024-08-22 16:57:59 -0500

    funcion para leer json

--- docs/script.js
+++ docs/script.js
@@ -1,24 +1,17 @@
-// Mientras no encuentro una solucion para CORS
+async function loadJSON(file) {
+    try {
+      const response = await fetch(file);
+      if (!response.ok) {
+        throw new Error("Network problems");
+      }
+      const data = await response.json();
+      return data;
 
-// fetch('https://github.com/code-diego/Horario/blob/main/web/data.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         //recorriendo todo la data
-//         for (let key in data) {
-//             if (data.hasOwnProperty(key)) {
-//                 console.log("Key : ", key)
-//                 let seccion = data[key].seccion;
-//                 for (let subKey in seccion) {
-//                     if (seccion.hasOwnProperty(subKey)) {
-//                         console.log("subKey : ", subKey);
-//                         console.log("Horario:", seccion[subKey].horario);
-//                         console.log("Aula:", seccion[subKey].aula);
-//                         console.log("Docente:", seccion[subKey].docente);
-//                     }
-//                 }
-//             }
-//         }
-//     })
-//     .catch(error => {
-//         console.error("error!! : ", error);
-//     })
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  }
+  
+var data = await loadJSON("data.json");
+  
